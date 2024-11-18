060b17c8f4d7f77f75beecf5b97ef8f0d6867253
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-11-18 10:21:29 -0400

    Idk

--- src/PlayerController.cs
+++ src/PlayerController.cs
@@ -5,53 +5,55 @@ public partial class PlayerController : CharacterBody3D
 {
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
-    private Vector2 inputDir;
+	private Vector2 inputDir;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector3 velocity = Velocity;
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
 
-        // Add the gravity.
-        if (!IsOnFloor())
-        {
-            velocity += GetGravity() * (float)delta;
-        }
+		inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 
-        velocity=HandleJump(velocity);
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
 
-        velocity=HandleWalk(velocity);
+		velocity=HandleJump(velocity);
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		velocity=HandleWalk(velocity);
 
-    private Vector3 HandleJump(Vector3 velocity)
-    {
-        // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-        {
-            velocity.Y = JumpVelocity;
-        }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 
-        return velocity;
-    }
+	private Vector3 HandleJump(Vector3 velocity)
+	{
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
 
-    private Vector3 HandleWalk(Vector3 velocity)
-    {
-        // Get the input direction and handle the movement/deceleration.
-        // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-        if (direction != Vector3.Zero)
-        {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-        }
+		return velocity;
+	}
 
-        return velocity;
-    }
+	private Vector3 HandleWalk(Vector3 velocity)
+	{
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		return velocity;
+	}
 }
