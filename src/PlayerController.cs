9cfaddfb1ee65ac41843f610274da0357419a969
mFriesen <144160470+mfriesen1024@users.noreply.github.com>
2024-11-19 13:02:55 -0400

    Setup camera for rotations

--- src/PlayerController.cs
+++ src/PlayerController.cs
@@ -1,16 +1,27 @@
 using Godot;
+using System;
 
 public partial class PlayerController : CharacterBody3D
 {
     public const float Speed = 5.0f;
     public const float JumpVelocity = 4.5f;
-    Vector2 inputDir;
+
+    Node3D camParent;
+
+    Vector2 moveDir;
+    Vector2 lookDir;
+
+    public override void _Ready()
+    {
+        camParent = (Node3D)GetChild(0);
+    }
 
     public override void _PhysicsProcess(double delta)
     {
         Vector3 velocity = Velocity;
 
-        inputDir = Input.GetVector("left", "right", "up", "down");
+        // Replace with proper input handling.
+        moveDir = Input.GetVector("left", "right", "up", "down");
 
         // Add the gravity.
         if (!IsOnFloor())
@@ -28,7 +39,7 @@ public partial class PlayerController : CharacterBody3D
 
     private Vector3 HandleJump(Vector3 velocity)
     {
-        // Handle Jump.
+        // Replace with proper input handling.
         if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
         {
             velocity.Y = JumpVelocity;
@@ -41,7 +52,7 @@ public partial class PlayerController : CharacterBody3D
     {
         // Get the input direction and handle the movement/deceleration.
         // As good practice, you should replace UI actions with custom gameplay actions.
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        Vector3 direction = (Transform.Basis * new Vector3(moveDir.X, 0, moveDir.Y)).Normalized();
         if (direction != Vector3.Zero)
         {
             velocity.X = direction.X * Speed;
